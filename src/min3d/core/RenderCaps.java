36fba9490589e49c71e0d76562183de3f1076b48
lfelarca <lfelarca@470a5c22-fb99-cafe-615f-f07c7e978966>
2010-05-24 23:05:22 +0000

    Updated RenderCaps to fix runtime error on some systems
    git-svn-id: http://min3d.googlecode.com/svn/trunk@60 470a5c22-fb99-cafe-615f-f07c7e978966

--- src/min3d/core/RenderCaps.java
+++ src/min3d/core/RenderCaps.java
@@ -3,6 +3,11 @@ package min3d.core;
 import java.nio.IntBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.opengles.GL11;
+
+import min3d.Min3d;
+
+import android.util.Log;
 
 /**
  * Simple static class holding values representing various capabilities of 
@@ -14,6 +19,7 @@ public class RenderCaps
 	private static float _openGlVersion;
 	private static boolean _isGl10Only;
 	private static int _maxTextureUnits;
+	private static int _maxTextureSize;
 	private static int _aliasedPointSizeMin;
 	private static int _aliasedPointSizeMax;
 	private static int _smoothPointSizeMin;
@@ -22,7 +28,7 @@ public class RenderCaps
 	private static int _aliasedLineSizeMax;
 	private static int _smoothLineSizeMin;
 	private static int _smoothLineSizeMax;
-	
+	private static int _maxLights;
 	
 	public static float openGlVersion()
 	{
@@ -79,6 +85,10 @@ public class RenderCaps
 		return _smoothLineSizeMax;
 	}
 	
+	public static int maxLights()
+	{
+		return _maxLights;
+	}
 	
 	/**
 	 * Called by Renderer.onSurfaceCreate() 
@@ -88,14 +98,22 @@ public class RenderCaps
 	    IntBuffer i;
 
 	    // OpenGL ES version
-		String strVersion = $gl.glGetString(GL10.GL_VERSION);
-	    String[] a = strVersion.split("\\ ");
-	    _openGlVersion = Float.parseFloat(a[a.length-1]);
-
+		if ($gl instanceof GL11) {
+			_openGlVersion = 1.1f;
+		}
+		else {
+			_openGlVersion = 1.0f;
+		}
+		
 	    // Max texture units
 		i = IntBuffer.allocate(1);
 		$gl.glGetIntegerv(GL10.GL_MAX_TEXTURE_UNITS, i);
 		_maxTextureUnits = i.get(0);
+		
+	    // Max texture size
+		i = IntBuffer.allocate(1);
+		$gl.glGetIntegerv(GL10.GL_MAX_TEXTURE_SIZE, i);
+		_maxTextureSize = i.get(0);
 		
 		// Aliased point size range
 		i = IntBuffer.allocate(2);
@@ -120,5 +138,15 @@ public class RenderCaps
 		$gl.glGetIntegerv(GL10.GL_SMOOTH_LINE_WIDTH_RANGE, i);
 		_smoothLineSizeMin = i.get(0);
 		_smoothLineSizeMax = i.get(1);
+		
+	    // Max lights
+		i = IntBuffer.allocate(1);
+		$gl.glGetIntegerv(GL10.GL_MAX_LIGHTS, i);
+		_maxLights = i.get(0);
+
+		Log.v(Min3d.TAG, "RenderCaps - openGLVersion: " + _openGlVersion);
+		Log.v(Min3d.TAG, "RenderCaps - maxTextureUnits: " + _maxTextureUnits);
+		Log.v(Min3d.TAG, "RenderCaps - maxTextureSize: " + _maxTextureSize);
+		Log.v(Min3d.TAG, "RenderCaps - maxLights: " + _maxLights);
 	}
 }
